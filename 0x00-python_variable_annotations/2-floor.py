9cb6bb340a815c844ba0e841f33f3f8c267b94d1
ochuks1 <cochuks@gmail.com>
2024-10-14 08:00:55 +0000

    task2 correction

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
+import math
 """
 Module for converting a float to a string.
 """
 
-def to_str(n: float) -> str:
-    """
-    Convert a float to a string.
 
-    Args:
-        n (float): The float to be converted.
-
-    Returns:
-        str: The float as a string.
-    """
-    return str(n)
+def floor(n: float) -> int:
+    """Returns the floor of the float."""
+    return math.floor(n)
