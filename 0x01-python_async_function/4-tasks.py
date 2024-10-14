55d290eff0f32b02f531de8b37bd3ff32ff3456e
ochuks1 <cochuks@gmail.com>
2024-10-14 12:08:19 +0000

    tasks?

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+A module that contains a function to wait for random delays using tasks.
+"""
+import asyncio
+from task_wait_random import task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list:
+    """
+    Executes task_wait_random n times with the specified max_delay.
+    
+    Args:
+        n (int): The number of tasks to spawn.
+        max_delay (int): The maximum delay value for the tasks.
+    
+    Returns:
+        list: A list of delays returned by the tasks.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays  # The delays will be returned as is
