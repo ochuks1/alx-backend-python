a58692edbd64124d5943516c48e6e257033a46ae
ochuks1 <cochuks@gmail.com>
2024-10-14 09:16:19 +0000

    advanced task1

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
 """
-This module defines a function safe_first_element.
-
-safe_first_element(lst: Sequence) -> Union[Any, None]:
-    Returns the first element of a sequence or None if the sequence is empty.
+This module defines a function safely_get_value.
 """
 
-from typing import Sequence, Any, Union
+from typing import Mapping, Any, Optional, TypeVar
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    Returns the first element of a sequence, otherwise returns None.
+T = TypeVar('T')
 
-    Args:
-        lst (Sequence[Any]): A sequence of elements.
-
-    Returns:
-        Union[Any, None]: The first element of the sequence or None if empty.
-    """
-    if lst:
-        return lst[0]
-    else:
-        return None
+def safely_get_value(dct: Mapping, key: Any, default: Optional[T] = None) -> Optional[T]:
+    if key in dct:
+        return dct[key]
+    return default
