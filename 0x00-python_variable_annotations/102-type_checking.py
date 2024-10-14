8f2b135a90f8b0c91f810e460eca7fa098876aed
ochuks1 <cochuks@gmail.com>
2024-10-14 09:44:50 +0000

    advanced task3

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -22,10 +22,3 @@ def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
         for _ in range(factor)
     ]
     return zoomed_in
-
-
-array = (12, 72, 91)
-
-zoom_2x = zoom_array(array)
-
-zoom_3x = zoom_array(array, 3)
