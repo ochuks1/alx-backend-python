0ad0b23ed2926cea8109c782680ba02dde207b49
ochuks1 <cochuks@gmail.com>
2024-10-14 08:24:29 +0000

    tasks 4 and 5

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -8,19 +8,15 @@ a: int = 1
 """
 Variable a is an integer with the value 1.
 """
-
-pi: float = 3.1416
+pi: float = round(3.1416, 2)
 """
 Variable pi is a float with the value 3.1416.
 """
-
 i_understand_annotations: bool = True
 """
 Variable i_understand_annotations is a boolean with the value True.
 """
-
 school: str = "Holberton"
 """
 Variable school is a string with the value "Holberton".
 """
-
