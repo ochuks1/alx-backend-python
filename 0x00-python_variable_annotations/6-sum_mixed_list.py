eccd97804586897e60b1af8357073c1e5faadd30
ochuks1 <cochuks@gmail.com>
2024-10-14 08:46:23 +0000

    task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,12 +5,13 @@ Module for getting string representation of a list.
 
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Takes a list of integers and floats and returns the sum as a float.
 
     Args:
-        mxd_lst (List[Union[int, float]]): A list containing integers and floats.
+        mxd_lst (List[Union[int, float]]): List of integers and floats.
 
     Returns:
         float: The sum of all numbers in the list as a float.
