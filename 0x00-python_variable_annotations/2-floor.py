945c34b00c63c8ad85768aae8b7b69c7b2c8b206
ochuks1 <cochuks@gmail.com>
2024-10-14 08:11:42 +0000

    task2 correction

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -6,5 +6,12 @@ This module contains a function that returns the floor of a float.
 
 
 def floor(n: float) -> int:
-    """Returns the floor of the float."""
+    """Returns the floor of the float.
+
+    Args:
+        n (float): The float number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to the float.
+    """
     return math.floor(n)
