3d1bd836dc1d96ff80b1429be164f796a1fbbe4c
ochuks1 <cochuks@gmail.com>
2024-10-15 12:39:01 +0000

    Tasksss

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -3,7 +3,6 @@
 This module defines a function task_wait_random which
 returns an asyncio.Task.
 """
-
 import asyncio
 import importlib.util
 spec = importlib.util.spec_from_file_location("wait_random", "./0-basic_async_syntax.py")
@@ -14,10 +13,8 @@ spec.loader.exec_module(wait_random)
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns an asyncio.Task that runs wait_random.
-
     Args:
         max_delay (int): Maximum delay for the wait_random coroutine.
-    
     Returns:
         asyncio.Task: A task for the wait_random coroutine.
     """
